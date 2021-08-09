d22ee5746a374477a65d43f90ac0145307c7b190
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-08-09 10:39:37 -0500

    Fix component name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import InfoHeader from "./components/infoHeader";
-import Computer from "./components/Computer";
+import Computer from "./components/computer";
 
 export default class App extends React.Component {
     render() {
